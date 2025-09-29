33dad57bdd0d2255177e981b0caa26dce4d4122b
Caua7s70 <cauamorato2602@gmail.com>
2025-09-29 10:34:43 +0100

    Add files via upload

---
+++ insulto.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Vc é tão feio mas tão feio que quando vc nasceu o médico que começou a chorar,";
+    std::cout<<" Vc devia ir numa floresta e passar de árvore em árvore pedindo desculpa por respirar o oxigênio que elas produzem";
+
+    return 0;
+}
